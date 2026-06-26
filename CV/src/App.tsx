ac78a1a51baf258f9f309f09d68acf9d11692176
Tuva Gyllensten <tuvagyllensten@gmail.com>
2026-06-26 12:34:44 +0200

    done with menu

--- CV/src/App.tsx
+++ CV/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css'
 import Menu from './components/Menu'
+import HomePage from './pages/Homepage'
 
 function App() {
   return (
-    <>
+    <Router>
       <Menu /> 
-      <h1>test</h1>
-    </>
+      <main style={{ marginLeft: '60px' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
--- CV/src/components/Menu.tsx
+++ CV/src/components/Menu.tsx
@@ -1,33 +1,56 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../style/MenuStyle.css'
 
 
 
 export default function Menu() {
+    const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <aside className='menu'>
-            <h1 className='logoText'>Tuvahilda</h1>
-            <nav>
-                <ul className='socialLinks'>
-                    <li><a href="https://github.com/gytu24nn"><i className="fa-brands fa-github"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/tuva-gyllensten-751a5833b/"><i className="fa-brands fa-linkedin"></i></a></li>
-                    <li><a href="mailto:tuvagyllensten@gmail.com"><i className="fa-solid fa-envelope"></i></a></li>
+        <>
+            <aside className='menu'>
+                <h1 className='logoText'>Tuvahilda</h1>
+                <div className='middleSection'>
+                    <nav>
+                        <ul className='socialLinks'>
+                            <li><a href="https://github.com/gytu24nn"><i className="fa-brands fa-github"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/tuva-gyllensten-751a5833b/"><i className="fa-brands fa-linkedin"></i></a></li>
+                            <li><a href="mailto:tuvagyllensten@gmail.com"><i className="fa-solid fa-envelope"></i></a></li>
+                        </ul>
+                    </nav>
+                    <div className='followMe'>
+                        <div className='followMeLine'/>
+                        <p className='followMeText'>Follow me</p>
+                    </div>
+                </div>
+
+                <div className='MenuBtn'>
+                    <button className='DarkAndLightModeBtn'>
+                        <i className="fa-solid fa-moon"></i>
+                    </button>
+
+                    <button className='MenuHamburgarBtn' onClick={() => setMenuOpen(!menuOpen)}>
+                        <i className={`fa-solid ${menuOpen ? 'fa-xmark' : 'fa-bars'}`}></i>
+                    </button>
+                </div>
+            </aside>
+
+            <nav className={`slideMenu ${menuOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><Link to="/" onClick={() => setMenuOpen(false)}>Hem</Link></li>                    <li></li>
+                    <li><Link to="/projekt" onClick={() => setMenuOpen(false)}>Projekt</Link></li>
+                    <li><Link to="/om-mig" onClick={() => setMenuOpen(false)}>Om mig</Link></li>
+                    <li><Link to="/tech-stack" onClick={() => setMenuOpen(false)}>Tech stack</Link></li>
+                    <li><Link to="/kontakt" onClick={() => setMenuOpen(false)}>Kontakt</Link></li>
                 </ul>
+
             </nav>
-            <div >
-                <div className='followMeLine'/>
-                <p className='followMeText'>Follow me</p>
-            </div>
 
-            <div className='MenuBtn'>
-                <button className='DarkAndLightModeBtn'>
-                    <i className="fa-solid fa-moon"></i>
-                </button>
+            {menuOpen && (
+                <div className='overlay' onClick={() => setMenuOpen(false)} />
+            )}
+        </>
 
-                <button className='MenuHamburgarBtn'>
-                    <i className="fa-solid fa-bars"></i>
-                </button>
-            </div>
-        </aside>
         
     );
 }
